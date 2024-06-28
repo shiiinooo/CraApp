abe86b93d24fe4997f5d6740df284f4f4c8c2436
AHMED-ELG <elghassibahmed@gmail.com>
2024-06-28 14:29:43 +0200

    FinishWhatWeCouldFinishInFront

--- CraApp.Web/Services/IServices/IMonthlyActivitiesService.cs
+++ CraApp.Web/Services/IServices/IMonthlyActivitiesService.cs
@@ -1,13 +1,11 @@
-﻿
-namespace CraApp.Web.Services
+﻿namespace CraApp.Web.Services;
+
+public interface IMonthlyActivitiesService
 {
-    public interface IMonthlyActivitiesService
-    {
-        Task<T> CreateAsync<T>(MonthlyActivityCreateDTO dto, string token);
-        Task<T> UpdateAsync<T>(MonthlyActivityUpdateDTO dto, string token);
-        Task<T> DeleteAsync<T>(int id, string token);
-        Task<T> GetAllAsync<T>(string token);
-        Task<T> GetAsync<T>(int id, string token);
-        Task<T> GetActivitiesByMonthAndUser<T>(int UserId, int Month, string token);
-    }
+    Task<T> CreateAsync<T>(MonthlyActivityCreateDTO dto, string token);
+    Task<T> UpdateAsync<T>(MonthlyActivityUpdateDTO dto, string token);
+    Task<T> DeleteAsync<T>(int id, string token);
+    Task<T> GetAllAsync<T>(string token);
+    Task<T> GetAsync<T>(int id, string token);
+    Task<T> GetActivitiesByMonthAndUser<T>(int UserId, int Month, string token);
 }
